0067f1838acff8e0f643a0e373d1315cc2677215
BlackNoizE404 <blacknoize404@gmail.com>
2021-09-16 17:16:58 -0400

    Sponsors section added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+//#region Bloqueando click derecho
+
+function disableIE() {
+    if (document.all) {
+        return false;
+    }
+}
+function disableNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which==2 || e.which==3) {
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = disableNS;
+} 
+else {
+    document.onmouseup = disableNS;
+    document.oncontextmenu = disableIE;
+}
+document.oncontextmenu=new Function("return false");
+
+//#endregion
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
